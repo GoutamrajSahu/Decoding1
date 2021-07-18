3f5ca848e26d93f25e5e4dec3947428f8594a07f
GoutamrajSahu <sahugoutamraj@gmail.com>
2021-07-18 14:45:39 +0530

    Update for better output.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,30 @@
 package com.company;
 import java.util.*;
-public class Main
-{
-    public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
-        System.out.print("Str = ");
-        String str = Sc.nextLine();
-        System.out.print("Key = ");
-        int K = Sc.nextInt();
-        String keyStr="";
 
-        for(int i = 0; i < str.length(); i++)
-        {
+class Main {
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        StringBuffer str = new StringBuffer(Sc.nextLine());
+        System.out.println("Enter the key: ");
+        int key = Sc.nextInt();
+        for(int i = 0; i < str.length(); i++){
             char ch = str.charAt(i);
-            if(ch == ' '){
-                keyStr = keyStr + ' ';
+            int ascii = ch;
+            if(ascii == 32){
                 continue;
             }
-            int Key = K%26;
-            int asciiOfCh1 = ch;
-            int asciiOfCh2 = asciiOfCh1 - Key;
-            if(asciiOfCh2 < 97 && asciiOfCh2 > 90){
-                int temp = asciiOfCh1-97;
-                Key = Key - temp;
-                asciiOfCh2 = 122 - (Key-1);
+            int newAscii = ascii-key;
+            if(newAscii<65){
+                int diff = 65 - newAscii;
+                newAscii = 90 - (diff-1);
             }
-            char chForReplace = (char)asciiOfCh2;
-
-            keyStr = keyStr + chForReplace;
+            else if(newAscii<97 && newAscii>90){
+                int diff = 97 - newAscii;
+                newAscii = 122 - (diff-1);
+            }
+            str.setCharAt(i,(char)newAscii);
         }
-        System.out.println(keyStr);
+        System.out.println(str);
     }
 }
